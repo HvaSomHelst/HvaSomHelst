e462d7ba0631675913425b9efc72634ab315b77a
Jorgba15 <jorgen.bordal@outlook.com>
2017-10-18 16:17:25 +0200

    12
    bsbsbsbb

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/:id', function(req, res) {
+  res.send('hello ' + req.params.id);
+});
+
+app.use(express.static("./public"));
+
+app.listen(process.env.PORT || 8080, function () { // do something with the port
+  console.log('Star wars');
+});
+
+console.log("noe tull");
+// skjønner ingenting
