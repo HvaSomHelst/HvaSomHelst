84f1adb227919c34bab91ce285de9f39cea72b7f
Erik Hellestol <31954581+ErikHellestol@users.noreply.github.com>
2017-11-29 12:30:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/:id', function(req, res) {
-  res.send('hello ' + req.params.id);
+
 });
 
 app.use(express.static("./public"));
